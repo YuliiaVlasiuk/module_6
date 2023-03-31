fb2001f1eba6f532f6330663eae47d3f8158409d
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-03-31 23:02:26 +0300

    2

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,29 @@
-import { Button } from "components/Button/Button";
-import css from "./StatusFilter.module.css";
+ import { Button } from "components/Button/Button";
+// import css from "./StatusFilter.module.css";
 
+// export const StatusFilter = () => {
+//   return (
+//     <div className={css.wrapper}>
+//       <Button>All</Button>
+//       <Button>Active</Button>
+//       <Button>Completed</Button>
+//     </div>
+//   );
+// };
+
+
+// Імпортуємо хук
+import { useSelector } from "react-redux";
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "../../redux/constants";
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(state => state.filters.status);
   return (
-    <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+    <div>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
